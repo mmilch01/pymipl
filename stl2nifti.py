af2cdc8163c2997048a89cc577feab9ee118da11
mmilch01 <mmilch01@gmail.com>
2022-07-06 11:58:12 -0500

    +stl2nifti

---
+++ stl2nifti.py
@@ -0,0 +1,61 @@
+'''
+Author: Mikhail Milchenko, mmilchenko@wustl.edu
+Copyright (c) 2022, Computational Imaging Lab, School of Medicine, Washington University in Saint Louis
+
+Redistribution and use in source and binary forms, for any purpose, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+'''
+
+import argparse,stltovoxel,numpy as np
+from nibabel.nifti1 import Nifti1Image,Nifti1Header
+import nibabel.nifti1
+from stl import mesh
+
+def stl2nifti(infile:str,outfile:str, resolution:int, padding_fraction:float):
+    #read stl mesh from file
+    mesh_obj=mesh.Mesh.from_file(infile)
+    
+    #make 'organized mesh', format to input to stltovoxel.
+    org_mesh = np.hstack((mesh_obj.v0[:, np.newaxis], mesh_obj.v1[:, np.newaxis], mesh_obj.v2[:, np.newaxis]))
+    meshdim=(np.amax(org_mesh,0)-np.amin(org_mesh,0))[0]  
+
+    #convert mesh to raster
+    vol,_,_=stltovoxel.convert_mesh(org_mesh,resolution=resolution)
+
+    #create output nifti file
+    nifti_affine=np.diag(np.array([meshdim[2]/vol.shape[0],meshdim[1]/vol.shape[1],meshdim[0]/vol.shape[2],1]))
+    
+    #pad raster with zeroes, pad size calculated as fraction of each linear dimension
+    sh=np.array(vol.shape);
+    pad=(sh*padding_fraction).astype(int)        
+    vol1=np.pad(vol, [(pad[0],pad[0]),(pad[1],pad[1]),(pad[2],pad[2])] )
+    
+    #write out
+    nifti_image=Nifti1Image(vol1,nifti_affine)
+    nibabel.nifti1.save(nifti_image,out)    
+
+def get_parser():
+    """
+    Parse input arguments.
+    """
+    parser = argparse.ArgumentParser(description='Convert stereolithography file to NIFTI binary mask')
+
+    # Positional arguments.
+    parser.add_argument("in_stl", help="Input stl file")
+    parser.add_argument("out_nii", help="Output NIFTI file")
+    parser.add_argument("--resolution", metavar="<int>",type=int,default=512,
+                        help="size in voxels of the biggest dimension of the output raster [512]")
+    parser.add_argument("--padding_fraction", metavar="<float>",type=float,default=0.25,
+                        help="fraction of the output raster size for bilateral zero padding [0.25]")
+    
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    p = get_parser()
+    p.in_stl
+    stl2nifti(p.in_stl,p.out_stl,p.resolution,p.padding_fraction)
+    write_rec_file(p.out_stl,main_extension='nii',infiles=[p.in_stl])
--- utils.py
+++ utils.py
@@ -1,4 +1,111 @@
-import argparse, getpass, cmdline_provenance as cmdprov, os.path
+import argparse, getpass, cmdline_provenance as cmdprov, os.path, nibabel as nib
+
+class IFH:
+    def __init__(self):
+        '''
+        interfile=""
+        version_of_keys="3.3"
+        conversion_program="tools_4dfp.py" #program that generated the file
+        name_of_data_file=""               #image data filename"
+        number_format="float"
+        imagedata_byte_order="littleendian"#bigendian
+        number_of_bytes_per_pixel=4
+        number_of_dimensions=4
+        matrix_size=np.array([0,0,0,0])    #image array dims
+        orientation=2                      #2: transverse; 3: coronal; 4: sagittal
+        scaling_factor=np.array([1.0,1.0,1.0]) #mm/pixel
+        mmpix=np.array([1.0,1.0,1.0])      #signed scaling factor
+        center=np.array([0.0,0.0,0.0])     #image center
+        '''
+        self._vals=dict()
+        self._int_scalars=['number of bytes per pixel','number of dimensions',\
+                              'orientation', 'matrix size [1]','matrix size [2]', \
+                             'matrix size [3]','matrix size [4]']
+        self._float_scalars=['scaling factor (mm/pixel) [1]','scaling factor (mm/pixel) [2]',\
+                             'scaling factor (mm/pixel) [3]']
+        self._float_arrays=['mmppix','center']
+        
+    def readIFH(self,root):
+        ifhfile=root+'.4dfp.ifh'
+        v,ins,fs,fa=self._vals,self._int_scalars,self._float_scalars,self._float_arrays
+        with open(ifhfile,'r') as ifh:
+            for line in ifh:
+                keyval=line.split(':=')
+                key,val=' '.join(keyval[0].strip().split(' ')),keyval[1].strip()
+                #print(key,val)
+                if key in ins:
+                    v[key]=int(val)
+                elif key in fs:
+                    v[key]=float(val)
+                elif key in fa:
+                    v[key]=[float(s) for s in val.split()]
+                else:
+                    v[key]=val              
+        #print (v)
+
+    def __str__(self):
+        v=self._vals
+        with io.StringIO() as out:
+            for key in v.keys(): print(key,v[key],file=out)
+            return out.getvalue()
+        
+    def writeIFH(self,root):
+        v,ins,fs,fa=self._vals,self._int_scalars,self._float_scalars,self._float_arrays        
+        ifhfile=root+'.4dfp.ifh'
+        print('writing',ifhfile)
+        with open(ifhfile,'w') as ifh:
+            for key in v.keys():
+                val=v[key]
+                if key in ins:
+                    ifh.write("{} := {}\n".format(key,val))
+                elif key in fs:
+                    ifh.write("{} := {:f}\n".format(key,val))
+                elif key in fa:
+                    ifh.write("{} := {}\n".format(key, ' '.join([str(f) for f in val]) ) )
+                else:
+                    ifh.write("{} := {}\n".format(key,val))
+            
+class _4DFP:
+    def __init__(self,root=None):
+        self.analyze_image=None
+        self.ifh=IFH()
+        self.infile_list=[]
+        if root is not None:
+            self.read(root)
+            self.infile_list=[root]
+            
+    def get_copy(self):
+        sai=self.analyze_image
+        newim=_4DFP()
+        newim.analyze_image=nib.AnalyzeImage(get_voxels().copy(),sai.affine,sai.header)
+        newim.ifh=self.ifh
+        newim.infile_list=self.infile_list
+        return newim
+        
+    def get_voxels(self):
+        return self.analyze_image.get_fdata() if self.analyze_image is not None else None
+    
+    def set_voxels(self,new_voxels):
+        ai=self.analyze_image
+        self.analyze_image=nib.AnalyzeImage(new_voxels,ai.affine,ai.header)
+        
+    def read(self, root):
+        try:
+            print('reading '+root)
+            self.analyze_image=nib.AnalyzeImage.load(root+".4dfp.img")
+            self.ifh.readIFH(root)
+        except Exception as e:
+            print (e.strerror)
+    
+    def write(self, root, infile_list=[]):
+        try:
+            print('writing',root+'.4dfp.img')
+            self.analyze_image.to_filename(root+'.4dfp.img')
+            self.ifh.writeIFH(root)
+            write_rec_file_4dfp(root,self.infile_list+infile_list)
+            
+        except Exception as e:
+            print(e)
 
 def get_rec_file_root(root,main_extension):
     '''
@@ -6,6 +113,7 @@ def get_rec_file_root(root,main_extension):
     root: input file with or without extension
     main_extension: input file extension to be included in rec file name, e.g. 'nii'
     '''
+    print('get_rec_file_root',root)
     if main_extension is None:
         return root+'.rec'
     
@@ -23,10 +131,40 @@ def get_inlog(root, inlogs):
     '''
     recfile=root+'.rec'
     if os.path.isfile(recfile):
-        inlogs[root]=cmdprov.read_log(recfile)
+        inlogs[root]=cmdprov.read_log(recfile)    
     else:
         print('log file {} does not exist'.format(recfile))
-
+        
+def get_inlog_4dfp(root, inlogs):
+    '''
+    Read input file log, if exists
+    root: input file
+    inlogs: dict containing all input logs
+    '''    
+    if os.path.isfile(root+'.rec'):
+        inlogs[root]=cmdprov.read_log(root+'.rec')
+    elif os.path.isfile(root+'.4dfp.rec'):
+        inlogs[root]=cmdprov.read_log(root+'.4dfp.rec')
+    elif os.path.isfile(root+'.4dfp.img.rec'):
+        inlogs[root]=cmdprov.read_log(root+'.4dfp.img.rec')    
+    else:
+        print('log file with root {} and rec extension does not exist'.format(root))
+        
+def write_rec_file_4dfp(root:str,infiles=[]):
+    '''
+    Write log file for input extension.
+    root: input file root
+    main_extension: input file extension
+    infiles: list of all input files that may have logs attached
+    '''    
+    inlogs={}
+    for file in infiles:
+        get_inlog_4dfp(file,inlogs) 
+    outlog=cmdprov.new_log(infile_logs=inlogs,extra_notes=["user: "+getpass.getuser(),"node: "+os.uname()[1]])    
+    outlog_file=root+'.4dfp.img.rec'    
+    print('writing',outlog_file)
+    cmdprov.write_log(outlog_file,outlog)
+    
 def write_rec_file(root:str,main_extension=None,infiles=[]):
     '''
     Write log file for input extension.
