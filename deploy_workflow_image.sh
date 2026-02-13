563c8cf07288a579f5d68e71dad7b4a9bacfc10e
Mikhail Milchenko <mmilchenko@wustl.edu>
2026-02-13 15:40:11 -0600

    +fixed pymipl path +more verbosity in main.sh, fixed base env

--- deploy_workflow_image.sh
+++ deploy_workflow_image.sh
@@ -1,4 +1,4 @@
 version=0.01
-docker tag xnat-env-bootstrap:latest mmilch01/xnat-ai-workflow:$version
+docker tag xnat-env-bootstrap:latest mmilch01/xnat-ai-workflow:latest
 docker login docker.io
-docker push mmilch01/xnat-ai-workflow:$version
+docker push mmilch01/xnat-ai-workflow:latest
--- xnat_env_bootstrap/main.sh
+++ xnat_env_bootstrap/main.sh
@@ -41,6 +41,8 @@ usage() {
 
 
 cmdline="$0 $*"
+echo "running command: $cmdline"
+
 
 if [[ $# -lt 4 ]]; then usage; exit -1; fi
 PROJECT="$1"; SUBJECT="$2"; EXPERIMENT="$3"; WORKFLOW_ID="$4"; shift 4
@@ -79,9 +81,11 @@ mkdir -p /workdir
 tmpdir="$(mktemp -d)"
 trap 'rm -rf "$tmpdir"' EXIT
 
+run_python="micromamba run -n base python" 
+
 # Download runtime environment from -microenv and import it into microconda. 
 env_loc="$tmpdir/$MICROENV_RESOURCE"
-python "$PYMIPL_DIR"/xnat_workflow/sync-resource-with-xnat.py \
+cmd=($run_python "$PYMIPL_DIR"/xnat_workflow/sync-resource-with-xnat.py \
     --xnat_host "$XNAT_HOST"                \
     --user "$XNAT_USER"                     \
     --password "$XNAT_PASS"                 \
@@ -89,7 +93,9 @@ python "$PYMIPL_DIR"/xnat_workflow/sync-resource-with-xnat.py \
     --project "$PROJECT"                    \
     --remote_resource "$MICROENV_RESOURCE"  \
     --local_resource "$env_loc"             \
-    --upload 0
+    --upload 0)
+echo "${cmd[@]}"
+"${cmd[@]}"
 
 if (( $? )); then exit_with_error "Failed to download microenv: $MICROENV_RESOURCE"; fi
 
@@ -99,6 +105,8 @@ mkdir -p /opt/packages/user
 env_repo_prefix="/opt/packages/user/env_repo"
 mkdir -p "$env_repo_prefix"
 rm -rf "$env_repo_prefix"/*
+
+echo tar -xzf "$env_loc" -C "$env_repo_prefix"
 tar -xzf "$env_loc" -C "$env_repo_prefix"
 if (( $? )); then exit_with_error "Failed to extract env tarball: $env_loc"; fi
 
@@ -115,6 +123,7 @@ if [[ -n "$REPO_GIT" ]]; then
     *) exit_with_error "-repo_git must be url@sha (SHA required)";;
   esac  
   [[ -n "$repo_url" && -n "$repo_sha" && "$repo_sha" != "$repo_url" ]] || exit_with_error "-repo_git must be url@sha (SHA required)"
+  echo git clone "$repo_url" "$alg_repo_prefix"
   git clone "$repo_url" "$alg_repo_prefix"
   (cd "$alg_repo_prefix" && git checkout "$repo_sha")
   if (( $? )); then exit_with_error "git checkout failed: $repo_url:$repo_sha"; fi
@@ -144,14 +153,16 @@ if [ -f "$job_sh_remote" ]; then
     if (( $? )); then exit_with_error "Failed to copy job script: $job_sh_remote"; fi
 
 elif [ -f "$job_yaml_remote" ]; then 
-    python "$PYMIPL_DIR"/xnat_workflow/run_container_adapter.py \
+    cmd=($run_python python "$PYMIPL_DIR"/xnat_workflow/run_container_adapter.py \
     --job_yaml "$job_yaml_remote" \
     --output_batch_file "$job_sh" \
     --input_mount_path "$INPUT_MOUNT" \
     --local_workdir_path "/workdir" \
     --pymipl_dir "$PYMIPL_DIR" \
     --project "$PROJECT" \
-    --main_repo_dir "$alg_repo_prefix"
+    --main_repo_dir "$alg_repo_prefix")
+    echo "${cmd[@]}"
+    "${cmd[@]}"
     if (( $? )); then exit_with_error "Failed to generate job script from: $job_yaml_remote"; fi
     generated=1
 else 
@@ -160,6 +171,7 @@ else
 fi
 #5. Execute main.sh. That script would upload outputs back to xnat on its own.
 chmod +x "$job_sh"
+echo "$job_sh"
 "$job_sh" 
 if (( $? )); then exit_with_error "Job script failed: $job_sh"; fi
 
@@ -192,8 +204,7 @@ cp -f "$job_sh" "$job_log_dir/job.sh"
 
 #7, However, we need to upload $job_sh to the runtime resource if it was generated, so do that.
 # Remember, resource mounts are not writable.
-
-python "$PYMIPL_DIR"/xnat_workflow/sync-resource-with-xnat.py \
+cmd=($run_python "$PYMIPL_DIR"/xnat_workflow/sync-resource-with-xnat.py \
     --xnat_host "$XNAT_HOST"                \
     --user "$XNAT_USER"                     \
     --password "$XNAT_PASS"                 \
@@ -203,8 +214,9 @@ python "$PYMIPL_DIR"/xnat_workflow/sync-resource-with-xnat.py \
     --experiment "$EXPERIMENT"              \
     --remote_resource "$WORKFLOW_ID"   \
     --local_resource "$job_log_dir"         \
-    --upload 1
-
+    --upload 1)
+echo "${cmd[@]}"
+"${cmd[@]}"
 if (( $? )); then exit_with_error "Failed uploading logs: $job_sh"; fi
 
 echo "Completed main functionality"
