e903e3a9e764e0d2ad5d984337d340ee407de8fe
Mikhail Milchenko <mmilchenko@wustl.edu>
2026-02-13 15:19:14 -0600

    +

--- deploy_workflow_image.sh
+++ deploy_workflow_image.sh
@@ -1,4 +1,4 @@
 version=0.01
-docker tag xnat_env_bootstrap:latest mmilch01/xnat-ai-workflow:$version
+docker tag xnat-env-bootstrap:latest mmilch01/xnat-ai-workflow:$version
 docker login docker.io
 docker push mmilch01/xnat-ai-workflow:$version
