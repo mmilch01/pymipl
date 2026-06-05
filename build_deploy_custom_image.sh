85f52837b7d02be2a394ad895299bb9fc9faad28
Mikhail Milchenko <mmilchenko@wustl.edu>
2026-06-05 14:37:40 -0500

    +environment variable argument

--- build_deploy_custom_image.sh
+++ build_deploy_custom_image.sh
@@ -21,6 +21,7 @@ Config file must define:
   REMOTE_IMAGE      remote Docker image name with tag, e.g. mmilch01/xnat-env:latest
   DOCKER_REPO       Docker repository, default: docker.io
   LOCAL_ENV_FOLDER  folder with user environment to embed into the Docker image.
+  ENV_VARS          list extra environment variables for ENV dockerfile directive, e.g. "VAR1=val1 VAR2=val2".
 EOF
 }
 
@@ -36,9 +37,19 @@ DOCKER_REPO="${DOCKER_REPO:-docker.io}"
 [ -n "${REMOTE_IMAGE:-}" ] || exit_with_error "REMOTE_IMAGE is not set in config file."
 [ -d "${LOCAL_ENV_FOLDER:-}" ] || exit_with_error "LOCAL_ENV_FOLDER $LOCAL_ENV_FOLDER must be a directory."
 
+tmp_dockerfile="$(mktemp "${TMPDIR:-/tmp}/Dockerfile.custom.XXXXXX")"
+#trap 'rm -f "$tmp_dockerfile"' EXIT
+echo "using temp dockerfile: $tmp_dockerfile"
+
+if [ -n "$ENV_VARS" ]; then 
+  cp "$script_dir/xnat_env_bootstrap/Dockerfile.custom" "$tmp_dockerfile"
+  printf '\nENV %s\n' "$ENV_VARS" >> "$tmp_dockerfile"
+fi
+
 echo "building image"
-echo docker build --tag "$LOCAL_IMAGE" --build-context user_env="$LOCAL_ENV_FOLDER" -f "$script_dir"/xnat_env_bootstrap/Dockerfile.custom $PYMIPL_DIR
-docker build --tag "$LOCAL_IMAGE" --build-context user_env="$LOCAL_ENV_FOLDER" -f "$script_dir"/xnat_env_bootstrap/Dockerfile.custom $PYMIPL_DIR || exit_with_error "Image build failed"
+
+echo docker build --tag "$LOCAL_IMAGE" --build-context user_env="$LOCAL_ENV_FOLDER" -f "$tmp_dockerfile" $PYMIPL_DIR
+docker build --tag "$LOCAL_IMAGE" --build-context user_env="$LOCAL_ENV_FOLDER" -f "$tmp_dockerfile" $PYMIPL_DIR || exit_with_error "Image build failed"
 
 echo "deploying image"
 docker tag "$LOCAL_IMAGE" "$REMOTE_IMAGE" || exit_with_error "docker tag failed: $LOCAL_IMAGE -> $REMOTE_IMAGE"
