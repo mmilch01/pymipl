512cbff0bc2f7b15311c5e23954b2366508d51e6
mmilch01 <mmilch01@gmail.com>
2026-06-24 16:09:55 -0500

    +fix typo

--- xnat_workflow/workflow_adapters.py
+++ xnat_workflow/workflow_adapters.py
@@ -47,9 +47,8 @@ def populate_job_fields(env_type, global_vars, workflow_id, data_dict, subject=N
             #e.g. /workdir/<subject>/<experiment>/... etc.
             job_workdir=job_workdir / val
     job['job_id'],job['job_title'],job['job_workdir']=job_id,job_title,job_workdir
-    if 'job_scan_id' in job.keys():
-        job['job_numeric_scan_id'] = "".join(c for c in job['job_scan_id'] if c.isdigit())
-s
+    if 'job_scan_id' in job:
+        job['job_numeric_scan_id'] = "".join(c for c in str(job['job_scan_id']) if c.isdigit())
     
     #Do not change the next two lines to correctly preserve the scan context
     job_scan_context=global_vars['g_input_mount_path'] / job['job_exp_label'] / 'SCANS'
